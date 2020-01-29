f96dbad5cc0b7f094ca3468f17fd0796e0167163
b10617028 <b10617028@gemail.yuntech.edu.tw>
2020-01-29 21:35:01 +0800

    13_

--- 13_RomanToInteger.cpp
+++ 13_RomanToInteger.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        map<char, int> mp = { {'M', 1000}, {'D', 500}, {'C', 100}, {'L', 50}, {'X', 10}, {'V', 5}, {'I', 1} };
+        unordered_map<char, int> mp = { {'M', 1000}, {'D', 500}, {'C', 100}, {'L', 50}, {'X', 10}, {'V', 5}, {'I', 1} };
         int sum=mp[s[s.length()-1]];
         for(int i=0;i<s.length()-1;i++){
             //cout<<sum<<endl;
