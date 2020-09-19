4c299a72451beafcbd668375f891fbfa37ad15ca
pcymichael <pcymichael@gmail.com>
2020-09-19 15:45:07 +0800

    delete old mode

--- 189_RotateArray.cpp
+++
@@ -1,9 +0,0 @@
-//O(n)
-class Solution {
-public:
-    void rotate(vector<int>& nums, int k) {
-        vector<int> temp=nums;
-        for(int i=0;i<nums.size();i++)
-            nums[(i+k)%nums.size()]=temp[i];
-    }
-};
